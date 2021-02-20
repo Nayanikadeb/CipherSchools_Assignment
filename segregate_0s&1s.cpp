ae8874a81a7095a3662601bfb1aaee271f0af797
Nayanikadeb <47624713+Nayanikadeb@users.noreply.github.com>
2021-02-20 21:52:49 +0530

    Add files via upload

---
+++ segregate_0s&1s.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int segregate(int arr[], int n)
+{
+	int count=0;
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]==0)
+		count++;
+	}
+	for(int i=0;i<count;i++)
+	arr[i]=0;
+	for(int i=count;i<n;i++)
+	arr[i]=1;
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+}
+int main()
+{
+	int arr[] = { 0, 1,0,1,1,0,1,1,0 ,0, 1, 1, 1 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+      
+    segregate(arr, n); 
+  
+      
+    return 0;
+}
